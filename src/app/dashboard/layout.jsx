fad1af6ba603fe931b5780a603e54da85da1638f
Ali ihsan Ertugrul <ertugrulali3538@gmail.com>
2024-01-10 00:14:19 +0300

    dynamic and static folders are created

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import DashboardHeader from "@/components/common/dashboard/header";
+
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <>
+      <DashboardHeader/>
+      <Container className="flex-grow-1">
+        {children}
+      </Container>
+      
+    </>
+  );
+};
+
+export default DashboardLayout;
--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,20 +1,26 @@
 "use client"
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import menuItems from "./menu.json"
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-dark" data-bs-theme="dark" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">AE SHOP</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            {
+              menuItems.map((item)=>(<Nav.Link key={item.id} href={item.url} as={Link}>{item.title}</Nav.Link>))
+            }
+            
+            
             
           </Nav>
         </Navbar.Collapse>
+        <Link href="/dashboard">Dashboard</Link>
       </Container>
     </Navbar>
   )
