0ee80bd1e0cd00a20391c21cf37b175cad16be51
Ali ihsan Ertugrul <ertugrulali3538@gmail.com>
2024-01-18 00:01:25 +0300

    form validation with actions

--- src/components/dashboard/products/btn-delete-product.jsx
+++ src/components/dashboard/products/btn-delete-product.jsx
@@ -1,12 +1,26 @@
 "use client"
+import { deleteProductAction } from "@/actions/product-actions";
 import React from "react";
 import { Button } from "react-bootstrap";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const BtnDeleteProduct = ({id}) => {
-    const handleClick = () => {
-        const resp=confirm("Are you sure to delete?")
-        if(!resp) return;
+
+
+    const handleClick = async() => {
+        const answer=confirm("Are you sure to delete?")
+        if(!answer) return;
+
+        try {
+
+          const res=await deleteProductAction(id);
+          if(res.errors.commonError){
+            alert (res.errors.commonError)
+          } 
+        } catch (error) {
+          console.log(error)
+        }
+        
         
     }
   return (
--- src/components/dashboard/products/new-product-form.jsx
+++ src/components/dashboard/products/new-product-form.jsx
@@ -3,30 +3,56 @@ import { createProductAction } from "@/actions/product-actions";
 import CancelButton from "@/components/common/form-controls/cancel-button";
 import SubmitButton from "@/components/common/form-controls/submit-button";
 import React from "react";
+import { useFormState } from "react-dom";
 import { Form } from "react-bootstrap";
-import "../../../actions/product-actions.js"
 
 const NewProductForm = () => {
+	const initialState = { message: null, errors: {} };
+
+	// useFormstate hook u ile form ve server actin arasinda  cift yonlu bir iletisim kanali olusur
+	// dispatch ile formdan server action a
+	// state ile server actin dan form a
+	const [state, dispatch] = useFormState(createProductAction, initialState);
+
+	const { title, description, price, category, image } = state.errors;
+
 	return (
-		<Form action={createProductAction}>
+		<>
+		{state.errors.common ? (
+				<div className="alert alert-danger">{state.errors.common}</div>
+			) : (
+				""
+			)}
+		<Form action={dispatch}>
 			<Form.Group className="mb-3">
 				<Form.Label>Title</Form.Label>
-				<Form.Control name="title" type="text" />
-				<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+				<Form.Control name="title" type="text" isInvalid={!!title} />
+				<Form.Control.Feedback type="invalid">
+					{title}
+				</Form.Control.Feedback>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Description</Form.Label>
-				<Form.Control name="description" as="textarea" rows={3} />
-				<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+				<Form.Control
+					name="description"
+					as="textarea"
+					rows={3}
+					isInvalid={!!description}
+				/>
+				<Form.Control.Feedback type="invalid">
+					{description}
+				</Form.Control.Feedback>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Price</Form.Label>
-				<Form.Control name="price" type="number" />
-				<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+				<Form.Control name="price" type="number" isInvalid={!!price} />
+				<Form.Control.Feedback type="invalid">
+					{price}
+				</Form.Control.Feedback>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Category</Form.Label>
-				<Form.Select name="category">
+				<Form.Select name="category" isInvalid={!!category}>
 					<option value="">Select</option>
 					<option value="Home">Home</option>
 					<option value="Computers">Computers</option>
@@ -34,12 +60,16 @@ const NewProductForm = () => {
 					<option value="Kids">Kids</option>
 					<option value="Grocery">Grocery</option>
 				</Form.Select>
-				<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+				<Form.Control.Feedback type="invalid">
+					{category}
+				</Form.Control.Feedback>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Image</Form.Label>
-				<Form.Control name="image" type="text" />
-				<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+				<Form.Control name="image" type="text" isInvalid={!!image} />
+				<Form.Control.Feedback type="invalid">
+					{image}
+				</Form.Control.Feedback>
 			</Form.Group>
 
 			{/* bu butonu ayrı bir component yapmayınca pending durumunu gösteremiyorum */}
@@ -47,6 +77,8 @@ const NewProductForm = () => {
 			<SubmitButton title="Create" />
 			<CancelButton />
 		</Form>
+		</>
+		
 	);
 };
 
